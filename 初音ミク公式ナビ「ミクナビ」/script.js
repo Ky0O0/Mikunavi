ccf6e016185cc6e8964cd22c0e603f730013d8d7
Ky000 <banana11122@wp.pl>
2023-02-15 21:27:20 +0100

    Add files via upload

---
+++ 初音ミク公式ナビ「ミクナビ」/script.js
@@ -0,0 +1,7 @@
+/*---List Script---*/
+function listOn(){
+	document.getElementById("language_ul_menu2").style.display = "block";
+}
+function listOff(){
+	document.getElementById("language_ul_menu2").style.display = "none";
+}
